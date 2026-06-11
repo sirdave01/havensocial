acc9731e0d74d7b84c11e767f51c50711b05a17a
sirdave01 <d08178084956@gmail.com>
2026-06-11 07:56:27 -0600

    added the homepage for havensocial media app

--- server.js
+++ server.js
@@ -2,11 +2,27 @@
 
 import express from "express";
 
+import { fileURLToPath } from 'url';
+
+import path from 'path';
+
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+/**
+  * Configure Express middleware
+  */
+
+// Serve static files from the public directory
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
 
