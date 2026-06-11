8a023f9507db863cdef32636fbe58d82400d0edf
sirdave01 <d08178084956@gmail.com>
2026-06-11 05:44:04 -0600

    added the node_modules, nodemon dependencies, changed from production to development, created package.json and nodemon.json with package-lock.json

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Hello, Node.js!");
+// importing express from express library
+
+import express from "express";
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+
+  res.send("Welcome to HavenSocial - the next-gen Social Media platform!");
+
+});
+
+app.listen(PORT, () => {
+
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+
+  console.log(`Environment: ${NODE_ENV}`);
+
+});
